cdac941c4e9d6fb4c1bdde772d97489d2b82c2d9
Barys Drahunou <barysdrahunou@gmail.com>
2024-10-30 16:15:53 +0300

    add common libs and logging support

--- src/main/java/com/senla/finance/starter/config/LoggingAutoConfiguration.java
+++ src/main/java/com/senla/finance/starter/config/LoggingAutoConfiguration.java
@@ -1,8 +1,11 @@
 package com.senla.finance.starter.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
+@ComponentScan(basePackages = { "com.senla.finance.starter" })
 public class LoggingAutoConfiguration {
 
 }
