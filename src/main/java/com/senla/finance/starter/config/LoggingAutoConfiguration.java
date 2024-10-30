cee0c544d2c297d9456e386c2a81c551c62d99f4
Barys Drahunou <barysdrahunou@gmail.com>
2024-10-30 14:31:24 +0300

    add common libs and logging support

--- src/main/java/com/senla/finance/starter/config/LoggingAutoConfiguration.java
+++ src/main/java/com/senla/finance/starter/config/LoggingAutoConfiguration.java
@@ -1,5 +1,8 @@
 package com.senla.finance.starter.config;
 
+import com.senla.finance.starter.aspect.ControllerLoggingAspect;
+import com.senla.finance.starter.aspect.DaoLoggingAspect;
+import com.senla.finance.starter.aspect.ServiceLoggingAspect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,5 +12,20 @@ public class LoggingAutoConfiguration {
     @Bean(name = "customBean")
     public String userManager() {
         return "UserManager bean start to create.";
+    }
+
+    @Bean
+    public ControllerLoggingAspect controllerLoggingAspect(){
+        return new ControllerLoggingAspect();
+    }
+
+    @Bean
+    public DaoLoggingAspect daoLoggingAspect(){
+        return new DaoLoggingAspect();
+    }
+
+    @Bean
+    public ServiceLoggingAspect serviceLoggingAspect(){
+        return new ServiceLoggingAspect();
     }
 }
